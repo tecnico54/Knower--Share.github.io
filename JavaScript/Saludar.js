7f2ef007728a5456db3d41c8502ffe916bde48ed
tecnico54 <tecnovalencia54@gmail.com>
2023-02-24 15:00:14 -0500

    Cursos

---
+++ JavaScript/Saludar.js
@@ -0,0 +1,4 @@
+{
+	console.log("Hola usuario, les agredesco por entrar a esta pagina de cursos via Yotube, estos cursos son: Programación, Medicina y un curso extra de: Ofimatica, Edicion etc..");
+	alert("Hola usuario, les agredesco por entrar a esta pagina de cursos via Yotube, estos cursos son: Programación, Medicina y un curso extra de: Ofimatica, Edicion etc..")
+}
